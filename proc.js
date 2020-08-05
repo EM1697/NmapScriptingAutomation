4af1ad149b4be5f326524f3177d5b2f8061e916f
Enrique Macias Murrieta <emacmur16@gmail.com>
2020-08-04 22:10:54 -0700

    JSON files of commands and txt for sites created. Commands executed succesfully

--- proc.js
+++ proc.js
@@ -1,33 +1,22 @@
 "use strict"
 const exec = require('child_process').exec;
 const fs = require('fs');
-const commands = require('/home/em16/commands.json');
-const sitesPlain = fs.readFileSync('/home/em16/sites.txt', 'utf8');
+const commands = require('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/commands.json');
+const sitesPlain = fs.readFileSync('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/sites.txt', 'utf8');
 let sites = sitesPlain.split('\n');
 sites.splice(sites.length-1);
-// console.log(commands[0].name);
+// console.log(sites);
+
 for(let site in sites)
 {
     for(let com in commands)
     {
-        console.log(`${sites[site]}-${commands[com].name}-${commands[com].command}`);
+        exec(`sudo nmap ${commands[com].command} ${sites[site]} -oN /home/em16/Documents/Test/${sites[site]}-${commands[com].name}.txt -oX /home/em16/Documents/Test/${sites[site]}-${commands[com].name}.xml`,
+        (e, stdout, stderr) => {
+            if (e) console.log(`ERROR: ${e.message}`);
+
+            console.log('Child Process STDOUT: '+stdout);
+            console.log('Child Process STDERR: '+stderr);
+        });
     }
 }
-
-
-// for (command in commands)
-// {
-//     for(site in sites)
-//     {
-//         sites.forEach(site => {
-//             exec(`sudo nmap -${command.command} ${site} -oN /home/em16/Documents/${site}-${command.name}.txt`, (e, stdout, stderr)=> {
-//                 if (e instanceof Error) {
-//                     console.error(e);
-//                     throw e;
-//                 }
-//                 console.log('stdout ', stdout);
-//                 console.log('stderr ', stderr);
-//         });
-//         });
-//     }
-// }
