cf281c0d7561f24cb630c113f433c459778ea4d6
Enrique Macias Murrieta <emacmur16@gmail.com>
2020-08-07 10:47:42 -0700

    Se agregaron los demas json de los demas scripts de nmap, el de intrusive y discovery tienen errores de estructura, se agregaó el archivo js para cada tipo de script

---
+++ dos.js
@@ -0,0 +1,22 @@
+"use strict"
+const exec = require('child_process').exec;
+const fs = require('fs');
+const commands = require('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/dos.json');
+const sitesPlain = fs.readFileSync('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/aplicativos.txt', 'utf8');
+let sites = sitesPlain.split('\n');
+sites.pop();
+
+
+for(let site in sites)
+{
+    for(let com in commands)
+    {
+        exec(`sudo nmap -Pn ${commands[com].command} ${sites[site]} -oN /home/em16/Documents/DoSApps/${sites[site]}-${commands[com].name}.txt -oX /home/em16/Documents/DoSApps/${sites[site]}-${commands[com].name}.xml`,
+        (e, stdout, stderr) => {
+            if (e) console.log(`ERROR: ${e.message}`);
+
+            console.log(`DONE: ${sites[site]}-${commands[com].name}`);
+            console.log(`ERROR: ${stderr}`);
+        });
+    }
+}
