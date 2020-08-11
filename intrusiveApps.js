9c8501ead95fdcba8103062910cd209de931c18a
Enrique Macias Murrieta <emacmur16@gmail.com>
2020-08-11 13:23:24 -0700

    Se corren todos los escripts seguidos y async

---
+++ intrusiveApps.js
@@ -0,0 +1,28 @@
+"use strict"
+const exec = require('child_process').exec;
+const fs = require('fs');
+const commands = require('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/intrusive.json');
+// const sitesPlain = fs.readFileSync('/home/em16/Documents/NmapScripts/NmapScriptingAutomation/aplicativos.txt', 'utf8');
+// let sites = sitesPlain.split('\n');
+// // sites.pop();
+// console.log(sites);
+
+
+for(let com in commands)
+{
+    // exec(`sudo nmap ${commands[com].command} ${sites[site]} -oN /home/em16/Documents/Test/${sites[site]}-${commands[com].name}.txt -oX /home/em16/Documents/Test/${sites[site]}-${commands[com].name}.xml`,
+    // (e, stdout, stderr) => {
+    //     if (e) console.log(`ERROR: ${e.message}`);
+
+    //     console.log(`DONE: ${sites[site]}-${commands[com].name}`);
+    //     console.log(`ERROR: ${stderr}`);
+    // });
+
+    exec(`sudo nmap -Pn ${commands[com].command} -iL aplicativos.txt -oN /home/em16/Documents/IntrusiveApps/${commands[com].name}.txt -oX /home/em16/Documents/IntrusiveApps/${commands[com].name}.xml`,
+    (e, stdout, stderr) => {
+        if (e) console.log(`ERROR: ${e.message}`);
+
+        console.log(`DONE: ${commands[com].name}`);
+        console.log(`ERROR: ${stderr}`);
+    });
+}
---
+++ runScripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo node intrusiveApps.js > /home/em16/Documents/IntrusiveApps.txt
+sudo node intrusivePort.js > /home/em16/Documents/IntrusivePort.txt
+sudo node dosApps.js > /home/em16/Documents/DoSApps.txt
+sudo node dosPort.js > /home/em16/Documents/DoSPort.txt
+sudo node fuzzerPort.js > /home/em16/Documents/FuzzerPort.txt
